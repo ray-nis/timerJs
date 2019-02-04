e5a2db623997ef4614ed78916cd9d2d6c235af67
ray-nis <reirei256@gmail.com>
2019-02-04 10:41:08 +0100

    reset

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 let startBtn = document.querySelector("#start");
 let stopBtn = document.querySelector("#stop");
+let resetBtn = document.querySelector("#reset");
 let clock = document.querySelector("#timer");
 let count;
 let sec = 0;
 let min = 0;
 let hour = 0;
+let currentCommand;
 
 startBtn.addEventListener("click", ()=> {
     count = setInterval(timer, 1000);
     startBtn.disabled = true;
+    currentCommand = "start";
 })
 
 stopBtn.addEventListener("click", ()=> {
     count = clearInterval(count);
     startBtn.disabled = false;
+    currentCommand = "stop";
+})
+
+resetBtn.addEventListener("click", ()=> {
+    min = 0; sec = 0; hour = 0;
+    if (currentCommand === "stop") {
+        clock.innerHTML = show(hour) + " : " + show(min) + " : " + show(sec);
+    }
 })
 
 function timer() {
